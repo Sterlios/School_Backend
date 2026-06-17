174076a94e4f06f4320112a883f7c84b9ae69016
Antonio <kuztosha@gmail.com>
2026-06-17 23:23:06 +0200

    Добавлен CourseTests

---
+++ tests/Domain.Tests/Courses/CourseTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+
+namespace Domain.Tests.Courses;
+
+public class CourseTests
+{
+    [Fact]
+    public void Create_Should_CreateCourse_WhenValidParametersAreProvided()
+    {
+        var course = Create();
+        course.Should().NotBeNull();
+    }
+
+    private object Create() => throw new NotImplementedException();
+}
